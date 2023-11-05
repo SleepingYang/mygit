e515b68c2affab16505ea66ef15c56a8f0da0cef
yang <602862268@qq.com>
2023-11-05 18:47:26 +0800

    add studentID

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,5 +6,6 @@ int main()
 	cout << "Hello World!" << endl;
 	cout << "Today is 11/05/2023." << endl;
 	cout << "This is Shenzhen University." << endl;
+	cout << "My student ID is 2021150220." << endl;
 	return 0;
 } 
