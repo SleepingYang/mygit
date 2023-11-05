0825f15cf9adc8b3163ddbef0f190e83c864b57c
Awenjy <89237778+Awenjy@users.noreply.github.com>
2023-11-05 22:56:38 +0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,5 +7,6 @@ int main()
 	cout << "Today is 11/05/2023." << endl;
 	cout << "This is Shenzhen University." << endl;
 	cout << "My student ID is 2021150220." << endl;
+	cout << "123";
 	return 0;
 } 
